f665de3f1baf45e93c38298dbd9150d20d4870aa
Atif Taskeen <atif.taskeen@venturedive.com>
2020-02-17 20:32:41 +0500

    Assignmet#1

---
+++ app.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('text.txt', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text'});
+    res.write(data);
+    res.end();
+  });
+}).listen(4000);
