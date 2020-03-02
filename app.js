dd88b487b1ee76b55c10277309aa5e7d9e6195ce
Atif Taskeen <atif.taskeen@venturedive.com>
2020-03-02 18:32:03 +0500

    Assignment#2

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('text.txt', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text'});
-    res.write(data);
-    res.end();
+const axios = require("axios");
+
+axios.get(`https://swapi.co/api/films/`).then(res => {
+  const films = res.data.results;
+  const fetchTitle = films.map(val => val.title);
+  console.log(fetchTitle);
+  const getSpecifictitl = films.filter(val => {
+    return val.title === "The Phantom Menace";
   });
-}).listen(4000);
+  console.log(getSpecifictitl);
+});
